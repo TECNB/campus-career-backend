476a55af2e2cbcce86b70c1aa5b003174a5e3498
TECNB <3489044730@qq.com>
2024-10-30 10:13:11 +0800

    Initial commit

---
+++ src/main/java/com/tec/campuscareerbackend/CampusCareerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tec.campuscareerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusCareerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusCareerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tec/campuscareerbackend/CampusCareerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tec.campuscareerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusCareerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
